6ce08fde86756e031886c82f6d7914dd7b849181
junya <junya@dentalx.jp>
2026-01-20 03:21:51 +0900

    Initial Commit

---
+++ SmokeCounter/SmokeCounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmokeCounterApp.swift
+//  SmokeCounter
+//
+//  Created by hondajunya on 2026/01/20.
+//
+
+import SwiftUI
+
+@main
+struct SmokeCounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
